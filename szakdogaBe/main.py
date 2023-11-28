f06e1af969081ac55ed4073dc35d85d3df223175
Sztremi Akos <sz.akos0101@gmail.com>
2023-11-28 08:51:52 +0100

    docker

--- szakdogaBe/main.py
+++ szakdogaBe/main.py
@@ -4,7 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 from scipy.integrate import solve_ivp
 from stable_baselines3 import TD3
-from environments import MassSpringDamper, tEnd, tStart, MultipleDataEnv, MultipleDataY1Env, x0, t, NormalizeData
+from environments import MassSpringDamper, tEnd, tStart, MultipleDataEnv, MultipleDataY1Env, x0, t, NormalizeData, dataLength
+from sklearn.metrics import r2_score
 
 class SystemRequest(BaseModel):
     name: str
@@ -14,6 +15,7 @@ class SystemRequest(BaseModel):
 class SystemResponse(BaseModel):
     expected: list[list[float]] = []
     predicted: list[list[float]] = []
+    r2Score: float
 
 origins = [
     "http://localhost",
@@ -39,8 +41,8 @@ async def predict(request: SystemRequest) -> SystemResponse:
         expected, predicted = MSDPredict(request.modelParams)
     else:
         expected, predicted = []
-            
-    return SystemResponse(expected=expected, predicted=predicted)
+    r2 = (r2_score(expected[0][:dataLength-1], predicted[0]) + r2_score(expected[1][:dataLength-1], predicted[1]))/2  
+    return SystemResponse(expected=expected, predicted=predicted, r2Score=r2)
 
 
 def MSDPredict(params):
@@ -68,4 +70,4 @@ def MSDPredict(params):
         predicted1.append(action[0])
 
     expected = solve_ivp(MassSpringDamper, [tStart, tEnd], x0, t_eval=t, args=params).y
-    return ([NormalizeData(expected[0]),NormalizeData(expected[1])], [predicted0, predicted1])
+    return (expected, [predicted0, predicted1])
